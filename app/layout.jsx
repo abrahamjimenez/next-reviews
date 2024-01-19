8ec6f267f14543a93bc6c8703df12881c3b38731
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-01-19 13:10:42 -0700

    Home page viewable

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,17 @@ export default function RootLayout({children}) {
     return (
         <html lang="en">
         <body>
-        {children}
+        <header>
+            [header]
+        </header>
+
+        <main>
+            {children}
+        </main>
+
+        <footer>
+            [footer]
+        </footer>
         </body>
         </html>
     );
---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <>
+            <h1>Indie gamer</h1>
+            <p>Best games in the world</p>
+        </>
+    );
+}
