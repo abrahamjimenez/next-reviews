d1049b7dbe9b85fd0b3aa96f6e91dc5adff224f4
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-01-19 13:45:19 -0700

    Add pages

---
+++ app/reviews/hollow-knight/page.tsx
@@ -0,0 +1,8 @@
+export default function HollowKnightPage() {
+    return (
+        <>
+            <h1>Hollow Knight</h1>
+            <p>This is the review</p>
+        </>
+    );
+}
---
+++ app/reviews/stardew-valley/page.tsx
@@ -0,0 +1,8 @@
+export default function StardewValleyPage() {
+    return (
+        <>
+            <h1>Stardew Valley</h1>
+            <p>Here we'll list all the reviews</p>
+        </>
+    );
+}
