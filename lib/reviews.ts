109d3e4ec7a46e62de8ecaa957a21acf65c8b9f5
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-01-23 21:12:32 -0700

    Get reviews from CMS

--- lib/reviews.ts
+++ lib/reviews.ts
@@ -1,13 +1,37 @@
 import {readdir, readFile} from "node:fs/promises";
 import matter from "gray-matter";
 import {marked} from "marked";
+import qs from "qs";
+
+const CMS_URL = "http://localhost:1337";
 
 export interface Review {
     title: string;
     date: string;
     image: string;
-    body: string;
     slug: string;
+}
+
+export interface FullReview extends Review {
+    body: string;
+}
+
+interface Attributes {
+    slug: string;
+    title: string;
+    subtitle: string;
+    publishedAt: string;
+    image: {
+        data: {
+            attributes: {
+                url: string;
+            };
+        };
+    };
+}
+
+interface ReviewData {
+    attributes: Attributes;
 }
 
 export async function getFeaturedReview(): Promise<Review> {
@@ -15,7 +39,7 @@ export async function getFeaturedReview(): Promise<Review> {
     return reviews[0];
 }
 
-export async function getReview(slug: string): Promise<Review> {
+export async function getReview(slug: string): Promise<FullReview> {
     const text = await readFile(`./content/reviews/${slug}.md`, "utf8");
     const {content, data: {title, date, image}} = matter(text);
     const body = await marked(content);
@@ -24,14 +48,22 @@ export async function getReview(slug: string): Promise<Review> {
 }
 
 export async function getReviews(): Promise<Review[]> {
-    const slugs = await getSlugs();
-    const reviews = [];
-    for (const slug of slugs) {
-        const review: Review = await getReview(slug);
-        reviews.push(review);
-    }
-    reviews.sort((a, b) => b.date.localeCompare(a.date));
-    return reviews;
+    const url = `${CMS_URL}/api/reviews?` +
+        qs.stringify({
+            fields: ["slug", "title", "subtitle", "publishedAt"],
+            populate: {image: {fields: ["url"]}},
+            sort: ["publishedAt:desc"],
+            pagination: {pageSize: 6},
+        }, {encodeValuesOnly: true});
+    console.log("getReviews", url);
+    const response = await fetch(url);
+    const {data} = await response.json();
+    return data.map(({attributes}: ReviewData): Review => ({
+        slug: attributes.slug,
+        title: attributes.title,
+        date: attributes.publishedAt.slice(0, "yyyy-mm-dd".length),
+        image: CMS_URL + attributes.image.data.attributes.url,
+    }));
 }
 
 export async function getSlugs(): Promise<string[]> {
